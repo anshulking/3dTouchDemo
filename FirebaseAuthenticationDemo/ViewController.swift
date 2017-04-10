f5a7861d5408c666a99c3b61bcdbc3bc4c8b0fc7
Anshul Shah <MAC238@anshul-mac238.local>
2017-04-10 11:47:18 +0530

    Initial Commit

---
+++ FirebaseAuthenticationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseAuthenticationDemo
+//
+//  Created by MAC238 on 4/10/17.
+//  Copyright © 2017 tatvasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
