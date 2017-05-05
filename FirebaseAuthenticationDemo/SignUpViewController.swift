e5a0d320e59d647ffec2902f5d9ed29d9f46e5ba
Anshul Shah <MAC238@anshul-mac238.local>
2017-05-05 10:44:58 +0530

    commit

---
+++ FirebaseAuthenticationDemo/SignUpViewController.swift
@@ -0,0 +1,66 @@
+//
+//  SignUpViewController.swift
+//  FirebaseAuthenticationDemo
+//
+//  Created by MAC238 on 4/10/17.
+//  Copyright © 2017 tatvasoft. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class SignUpViewController: UIViewController {
+
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var confirmPasswordTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClickSignUp(_ sender: UIButton) {
+        if emailTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }else if(passwordTextField.text != confirmPasswordTextField.text){
+            let alertController = UIAlertController(title: "Error", message: "Password and confirm password should be same", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+
+        }else {
+            FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    _ = self.navigationController?.popViewController(animated: true)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func onClickLogin(_ sender: UIButton) {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+}
--- FirebaseAuthenticationDemo/ViewController.swift
+++ FirebaseAuthenticationDemo/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
-
+import Firebase
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
 class ViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +34,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func onClickLogin(_ sender: Any) {
+        if self.emailTextField.text == "" || self.passwordTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            FIRAuth.auth()?.signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    print(user!.uid)
+                    self.performSegue(withIdentifier: "segueSuccessfullyLogin", sender: self)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    @IBAction func onClickSignUp(_ sender: Any) {
+        self.performSegue(withIdentifier: "segueLoginToSignUp", sender: self)
+    }
+    
+    
 }
+
 
